d00c6000d60a2eecb1d275d295ddde2b6d5331b5
avelar <fortunato.avelar@gmail.com>
2016-09-30 10:09:24 -0300

    lib animatecss added

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,35 +1,38 @@
-var YY = 2016;
-var MM = 12;
-var DD = 10;
-var HH = 18;
-var MI = 00;
-var SS = 00;
+$( document ).ready(function() {
+  var YY = 2016;
+  var MM = 12;
+  var DD = 10;
+  var HH = 18;
+  var MI = 00;
+  var SS = 00;
 
-function atualizaContador()
-{
-var hoje = new Date();
-var futuro = new Date(YY,MM-1,DD,HH,MI,SS);
-var ss = parseInt((futuro - hoje) / 1000);
-var mm = parseInt(ss / 60);
-var hh = parseInt(mm / 60);
-var dd = parseInt(hh / 24);
-ss = ss - (mm * 60);
-mm = mm - (hh * 60);
-hh = hh - (dd * 24);
-var faltam = '';
-faltam += (dd && dd > 1) ? dd+' dias ' : (dd==1 ? '1 dia, ' : '');
-faltam += (toString(hh).length) ? hh+' hr, ' : '';
-faltam += (toString(mm).length) ? mm+' min e ' : '';
-faltam += ss+' seg';
+  function atualizaContador()
+  {
+  var hoje = new Date();
+  var futuro = new Date(YY,MM-1,DD,HH,MI,SS);
+  var ss = parseInt((futuro - hoje) / 1000);
+  var mm = parseInt(ss / 60);
+  var hh = parseInt(mm / 60);
+  var dd = parseInt(hh / 24);
+  ss = ss - (mm * 60);
+  mm = mm - (hh * 60);
+  hh = hh - (dd * 24);
+  var faltam = '';
+  faltam += (dd && dd > 1) ? dd+' dias ' : (dd==1 ? '1 dia, ' : '');
+  faltam += (toString(hh).length) ? hh+' hr, ' : '';
+  faltam += (toString(mm).length) ? mm+' min e ' : '';
+  faltam += ss+' seg';
 
- if (dd+hh+mm+ss > 0)
- {
-  document.getElementById('timer').innerHTML = faltam;
-  setTimeout(atualizaContador,1000);
- }
- else
- {
-  document.getElementById('timer').innerHTML = 'É hoje!!';
-  setTimeout(atualizaContador,1000);
- }
-}
+   if (dd+hh+mm+ss > 0)
+   {
+    document.getElementById('timer').innerHTML = faltam;
+    setTimeout(atualizaContador,1000);
+   }
+   else
+   {
+    document.getElementById('timer').innerHTML = 'É hoje!!';
+    setTimeout(atualizaContador,1000);
+   }
+  }
+  atualizaContador();
+});
